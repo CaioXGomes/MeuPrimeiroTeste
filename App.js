623e99862a7899a4631410ffc3caf2d0b5b25d90
unknown <you@example.com>
2023-02-14 10:38:35 -0300

    Adição texts botões

--- App.js
+++ App.js
@@ -5,6 +5,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Ola</Text>
+      <Text>Open up APP.jS</Text>
+      <Text>adicionando uma coisa</Text>
+      <Text>adicionando uma caixa</Text>
+      <Button title="enviar"></Button>
       <StatusBar style="auto" />
     </View>
   );
